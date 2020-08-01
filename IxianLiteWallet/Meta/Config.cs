d86589190d99ed6f99874353e6608eb67c40a6bf
MarkoAT <no-reply@ixian.io>
2020-08-01 08:17:30 +0200

    - updated bakedBlockHeight

--- IxianLiteWallet/Meta/Config.cs
+++ IxianLiteWallet/Meta/Config.cs
@@ -12,10 +12,10 @@ namespace LW.Meta
         // Block height at which the current version of Spixi was generated
         // Useful for optimized block header sync
         // Note: Always round last block height to 1000 and subtract 1 (i.e. if last block height is 33234, the correct value is 32999)
-        public static ulong bakedBlockHeight = 1256999;
+        public static ulong bakedBlockHeight = 1262999;
 
         // Block checksum (paired with bakedBlockHeight) of bakedBlockHeight
         // Useful for optimized block header sync
-        public static byte[] bakedBlockChecksum = Crypto.stringToHash("490e4d45bbe16b350674c53fbe053233eb90de40f9dc1bfa146c546dac2f01dc46cd4bdba342981b39c375e4");
+        public static byte[] bakedBlockChecksum = Crypto.stringToHash("456357156126adfa5b20ee46169de8c68545a2570835a92898933ad39f1855d34de38ff76b08edf4a053fbe7");
     }
 }

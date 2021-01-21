2935eaf579d6e2e5a055f752f22cc8b5b25d3d34
MarkoAT <no-reply@ixian.io>
2021-01-21 21:02:24 +0100

    xlwc-0.7.6b

--- IxianLiteWallet/Meta/Config.cs
+++ IxianLiteWallet/Meta/Config.cs
@@ -5,6 +5,6 @@
         public static string walletFile = "ixian.wal";
         public static bool onlyShowAddresses = false;
 
-        public static readonly string version = "xlwc-0.7.6a"; // LiteWallet version
+        public static readonly string version = "xlwc-0.7.6b"; // LiteWallet version
     }
 }

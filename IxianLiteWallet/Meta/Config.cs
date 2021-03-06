28cac99bc1e536e68df34e3b890828de6cc056f4
MarkoAT <no-reply@ixian.io>
2021-03-06 23:03:07 +0100

    xlwc-0.8.0c - block v9 support

--- IxianLiteWallet/Meta/Config.cs
+++ IxianLiteWallet/Meta/Config.cs
@@ -5,6 +5,6 @@
         public static string walletFile = "ixian.wal";
         public static bool onlyShowAddresses = false;
 
-        public static readonly string version = "xlwc-0.8.0a"; // LiteWallet version
+        public static readonly string version = "xlwc-0.8.0c"; // LiteWallet version
     }
 }

3ebe7c691683f12074b9e9dd3fe22b9fcabd15a2
MarkoAT <no-reply@ixian.io>
2022-06-11 14:39:53 +0200

    compatibility improvements

--- IxianLiteWallet/Meta/Node.cs
+++ IxianLiteWallet/Meta/Node.cs
@@ -193,8 +193,8 @@ namespace LW.Meta
             {
                 using (BinaryWriter writer = new BinaryWriter(mw))
                 {
-                    writer.WriteIxiVarInt(IxianHandler.getWalletStorage().getPrimaryAddress().addressNoChecksum.Length);
-                    writer.Write(IxianHandler.getWalletStorage().getPrimaryAddress().addressNoChecksum);
+                    writer.WriteIxiVarInt(IxianHandler.getWalletStorage().getPrimaryAddress().addressWithChecksum.Length);
+                    writer.Write(IxianHandler.getWalletStorage().getPrimaryAddress().addressWithChecksum);
                     NetworkClientManager.broadcastData(new char[] { 'M', 'H' }, ProtocolMessageCode.getBalance2, mw.ToArray(), null);
                 }
             }
